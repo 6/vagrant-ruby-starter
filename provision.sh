79ce67731f2a1b164381b020f0af4ff05bf2b617
Peter Graham <peterghm@gmail.com>
2014-01-10 00:19:54 -0800

    fix an issue with RVM

--- provision.sh
+++ provision.sh
@@ -6,12 +6,12 @@ apt-get update
 apt-get -y install build-essential git-core python-software-properties
 apt-get -y install vim emacs curl
 
-# Install RVM
+# Install RVM and enable it
 curl -sSL https://get.rvm.io | bash -s stable
+source /usr/local/rvm/scripts/rvm
 
 # Ruby installation and setup
 rvm install $1
-source /usr/local/rvm/scripts/rvm
 rvm --default use $1
 gem install bundler
 

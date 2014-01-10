946154d950158eb03f688aa80ce75113e9758928
Peter Graham <peter@1000memories.com>
2014-01-10 14:34:07 -0800

    Use ansible for provisioning
    Since ansible does not run on Windows, install ansible on the VM and
    provision from there

--- Vagrantfile
+++ Vagrantfile
@@ -2,10 +2,10 @@
 # vi: set ft=ruby :
 
 VAGRANTFILE_API_VERSION = "2"
-RUBY_TO_INSTALL = "2.1.0"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "vagrant-ruby-starter"
   config.vm.box_url = "http://files.vagrantup.com/precise32.box"
-  config.vm.provision :shell, :path => "provision.sh", :args => RUBY_TO_INSTALL
+  config.vm.synced_folder "ansible/", "/ansible"
+  config.vm.provision :shell, :path => "provision.sh"
 end
--- provision.sh
+++ provision.sh
@@ -2,18 +2,12 @@
 
 apt-get update
 
-# Essential software
-apt-get -y install build-essential git-core python-software-properties
-apt-get -y install vim emacs curl
+# Install ansible
+apt-get -y install python-software-properties
+add-apt-repository -y ppa:rquillo/ansible
+apt-get -y install ansible
+echo localhost > /etc/ansible/hosts
 
-# Install RVM and enable it
-curl -sSL https://get.rvm.io | bash -s stable
-source /usr/local/rvm/scripts/rvm
-
-# Ruby installation and setup
-rvm install $1
-rvm --default use $1
-gem install bundler
-
-# Heroku toolbelt
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+# Use ansible to install remaining dependencies
+export PYTHONUNBUFFERED=1
+ansible-playbook /ansible/playbook.yml --connection=local
